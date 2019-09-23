717c5647b2d2de5b3721945bc20848638cacea17
santhosh <chandrasanthosh4@gmail.com>
2019-09-23 16:27:08 +0530

    deleting the projects

--- ExportDbToFileBatchJob/src/main/java/com/training/spring/batch/dao/CustomerDao.java
+++
@@ -1,9 +0,0 @@
-package com.training.spring.batch.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.training.spring.batch.entity.Customer;
-
-public interface CustomerDao extends JpaRepository<Customer, Long> {
-
-}
